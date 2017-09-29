201e4a472ecd50556ae66e01713b0d6b364f0d22
jfoucher <jfoucher@6px.eu>
2017-09-29 21:18:20 +0200

    Update manifest with release

---
+++ release.php
@@ -0,0 +1,39 @@
+#!/usr/bin/env php
+<?php
+
+if (!isset($argv[1])) {
+    echo 'Please specify the tag for this release';
+    exit(1);
+}
+
+$tag = $argv[1];
+
+shell_exec('git tag '.$tag);
+shell_exec('./build.sh');
+shell_exec('git tag -d'.$tag);
+
+
+//update manifest.json
+
+$manifest = json_decode(file_get_contents('./docs/manifest.json'));
+$manifest[] = [
+    "name" => "stati.phar",
+    "sha1" => sha1(file_get_contents('./build/stati.phar')),
+    "url" => "https://raw.githubusercontent.com/jfoucher/stati/".$tag."/build/stati.phar",
+    "version" => $tag,
+    "plugins" => [
+        [
+            "name" => "paginator",
+            "url" => "https://raw.githubusercontent.com/jfoucher/stati/".$tag."/build/plugins/paginator.phar"
+        ],
+        [
+            "name" => "related",
+            "url" => "https://raw.githubusercontent.com/jfoucher/stati/".$tag."/build/plugins/related.phar"
+        ]
+    ]
+];
+
+file_put_contents('./docs/manifest.json', json_encode($manifest));
+
+shell_exec('git commit -m "Creating release with tag'.$tag.'" && git push');
+shell_exec('git tag '.$tag.' && git push --tags');
