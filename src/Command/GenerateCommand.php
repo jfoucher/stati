845a9e4144c2a6592076dfe167aa80f20ff1d08b
jfoucher <jfoucher@6px.eu>
2017-10-06 03:35:15 +0200

    Creating release with tag 0.3.16

--- src/Command/GenerateCommand.php
+++ src/Command/GenerateCommand.php
@@ -171,7 +171,7 @@ class GenerateCommand extends Command
             var_dump(dirname($dir) . '/../../../bin/' . $requestedPlugin . '.phar');
             if (is_file(dirname($dir) . '/../../../bin/' . $requestedPlugin . '.phar')) {
                 include('phar://' . dirname($dir) . '/../../../bin/' . $requestedPlugin . '.phar/vendor/autoload.php');
-            } else if (is_file(dirname($dir) . '//' . $requestedPlugin . '.phar')) {
+            } else if (is_file(dirname($dir) . '/' . $requestedPlugin . '.phar')) {
                 include('phar://' . dirname($dir) . '/' . $requestedPlugin . '.phar/vendor/autoload.php');
             }
         } else {
