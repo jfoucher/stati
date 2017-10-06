f88497ac7198c918b12161b4383f41da672fa3cb
jfoucher <jfoucher@6px.eu>
2017-10-06 02:23:48 +0200

    CS fix

--- src/Install.php
+++ src/Install.php
@@ -1,5 +1,13 @@
 <?php
 
+/*
+ * This file is part of the Stati package.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @package Stati
+ */
 
 namespace Stati;
 
@@ -15,15 +23,14 @@ class Install
         //Copy bin to /usr/local/bin
         try {
             $fileSystem->copy(__DIR__ . '/../build/stati.phar', '/usr/local/bin/stati');
-        } catch(IOException $e) {
+        } catch (IOException $e) {
             echo $e->getMessage();
         }
         // Copy bin to above vendor folder
         try {
             $fileSystem->copy(__DIR__ . '/../build/stati.phar', __DIR__ . '/../../../bin/stati');
-        } catch(IOException $e) {
+        } catch (IOException $e) {
             echo $e->getMessage();
         }
-
     }
 }
