e71b44018c44888dbdfdc301f94e1a31df2def9d
jfoucher <jfoucher@6px.eu>
2017-10-06 02:20:55 +0200

    Copy stati.bin

--- src/Install.php
+++ src/Install.php
@@ -5,14 +5,22 @@ namespace Stati;
 
 use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\Filesystem\Exception\IOException;
+use Composer\Script\Event;
 
 class Install
 {
-    public static function postUpdate()
+    public static function postUpdate(Event $event)
     {
         $fileSystem = new Filesystem();
+        //Copy bin to /usr/local/bin
         try {
-            $fileSystem->copy('./build/stati.phar', '../bin/');
+            $fileSystem->copy(__DIR__ . '/../build/stati.phar', '/usr/local/bin/stati');
+        } catch(IOException $e) {
+            echo $e->getMessage();
+        }
+        // Copy bin to above vendor folder
+        try {
+            $fileSystem->copy(__DIR__ . '/../build/stati.phar', __DIR__ . '/../../../bin/stati');
         } catch(IOException $e) {
             echo $e->getMessage();
         }
