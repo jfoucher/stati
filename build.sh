2f6286c18cb3598b9c2b47aeaf953a2afdcd51c6
jfoucher <jfoucher@6px.eu>
2017-10-09 16:11:59 +0200

    Remove plugins, update build script

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-composer --no-dev update && box build && cp ./build/stati.phar ./build/stati && cd ../stati-paginate-plugin/ && box build && cd ../stati/ && cd ../stati-related-plugin/ && box build && cd ../stati/ && cd ../stati-categories-plugin/ && box build && cd ../stati/ && cp ../stati-paginate-plugin/build/paginate.phar ./build/ && cp ../stati-related-plugin/build/related.phar ./build/ && cp ../stati-categories-plugin/build/categories.phar build/ && cd ./build/ && zip stati.zip *.phar stati && cd ../ && composer update
+composer --no-dev update && box build && cp ./build/stati.phar ./build/stati && cd ./build/ && zip stati.zip stati && cd ../ && composer update
+
