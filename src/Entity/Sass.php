dec02b120583b6392a07243d1e25b3c5d1c42da4
jfoucher <jfoucher@6px.eu>
2017-10-16 00:10:06 +0200

    Refactor

--- src/Entity/Sass.php
+++ src/Entity/Sass.php
@@ -21,6 +21,7 @@ use Stati\Liquid\Template\Template;
 use Stati\Liquid\TemplateEvents;
 use Stati\Parser\ContentParser;
 use Liquid\Liquid;
+use Stati\Site\Site;
 use Stati\Site\SiteEvents;
 
 class Sass extends Page
@@ -40,12 +41,13 @@ class Sass extends Page
         if (!is_dir($cacheDir)) {
             mkdir($cacheDir, 0755, true);
         }
-        $this->cacheFileName = $this->getSlug() . '-' . sha1($content . implode('', Liquid::arrayFlatten($this->getFrontMatter())) . $this->site->__toString());
+        $this->cacheFileName = $this->getSlug() . '-' . sha1($content . implode('', Site::arrayFlattenToString($this->getFrontMatter())) . $this->site->__toString());
         if (is_file($cacheDir . '/' . $this->cacheFileName)) {
             return file_get_contents($cacheDir . '/' . $this->cacheFileName);
         }
 
-        $sassDir = ($this->site->sass && isset($this->site->sass['sass_dir'])) ? $this->site->sass['sass_dir'] : './_sass/';
+        $sassDir = $this->getSassDir();
+
         $sassStyle = ($this->site->sass && isset($this->site->sass['style'])) ? $this->site->sass['style'] : 'nested';
 
         if (strpos('compressed', $sassStyle) !== false) {
@@ -55,35 +57,40 @@ class Sass extends Page
         $parser = new ContentParser();
         $contentPart = $parser::parse($content);
 
+        $liquidParsed = $this->getLiquidParsed($contentPart);
+
+        file_put_contents($cacheDir . '/' . $this->cacheFileName . '_liquid', $liquidParsed);
+
+        $this->content = shell_exec('scss --load-path='.$sassDir.' --style='.$sassStyle.' --compass <' . escapeshellarg($cacheDir . '/' . $this->cacheFileName . '_liquid'));
+
+        file_put_contents($cacheDir . '/' . $this->cacheFileName, $this->content);
+
+        return $this->content;
+    }
+
+    private function getLiquidParsed($content)
+    {
         //Parse with Liquid
 
         $template = new Template(Liquid::get('INCLUDE_PREFIX'));
 
         $this->site->getDispatcher()->dispatch(TemplateEvents::WILL_PARSE_TEMPLATE, new WillParseTemplateEvent($this->site, $template));
         try {
-            $template->parse($contentPart);
+            $template->parse($content);
 
             $vars = [
                 'site' => $this->site
             ];
 
             $this->site->getDispatcher()->dispatch(TemplateEvents::SETTING_TEMPLATE_VARS, new SettingTemplateVarsEvent($this->site, $vars, $this));
-            $liquidParsed = $template->render($vars);
+            return $template->render($vars);
         } catch (LiquidException $err) {
             $this->site->getDispatcher()->dispatch(SiteEvents::CONSOLE_OUTPUT, new ConsoleOutputEvent(
                 'error',
                 ['Could not render the file '.$this->getFile()->getRelativePathname().' '.$err->getMessage()]
             ));
-            $liquidParsed = '';
+            return '';
         }
-        file_put_contents($cacheDir . '/' . $this->cacheFileName . '_liquid', $liquidParsed);
-        $this->content = shell_exec('scss --load-path='.$sassDir.' --style='.$sassStyle.' --compass < ' .$cacheDir . '/' . $this->cacheFileName . '_liquid');
-        file_put_contents($cacheDir . '/' . $this->cacheFileName, $this->content);
-        return $this->content;
-    }
-
-    private function getLiquidParsed()
-    {
     }
 
     public function getOutputPath()
@@ -93,5 +100,14 @@ class Sass extends Page
             $extension = 'css';
         }
         return str_replace('//', '/', '/'.$this->file->getRelativePath().'/'.pathinfo($this->file->getBasename(), PATHINFO_FILENAME).'.'.$extension);
+    }
+
+    private function getSassDir()
+    {
+        $sassDir = isset($this->site->getConfig()['sass']['sass_dir']) ? $this->site->getConfig()['sass']['sass_dir'] : '_sass';
+
+        $sassDir = str_replace('//', '/', $this->site->getConfig()['source'] . '/' . $sassDir);
+
+        return $sassDir;
     }
 }
